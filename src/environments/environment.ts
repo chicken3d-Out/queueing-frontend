7669fe47c990b56d480e27772214ddc2044f6bc2
chicken3d-Out <vincent.ontuca@evsu.edu.ph>
2026-08-12 23:13:32 +0800

    environment vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000',
+  apiUrl: 'https://queueing-backend-9mzs.onrender.com/api',
+  socketUrl: 'https://queueing-backend-9mzs.onrender.com',
 };
